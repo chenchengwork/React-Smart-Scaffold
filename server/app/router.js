a32bb3cbf1c091c6aa5134b9820a50032f9530ef
chencheng <dainel_txy@163.com>
2019-04-01 09:23:27 +0800

    添加graphql

--- server/app/router.js
+++ server/app/router.js
@@ -5,5 +5,9 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+
   router.get('/', controller.home.index);
+  router.post('/api/auth/login', 'auth.login');
+  router.post('/api/auth/register', 'auth.register');
+  router.resources('users', '/api/users', controller.user);
 };
